ac93b4b23571c4c9236121efb464f8a2e4858391
Guest User <Guest@TCC-09.local>
2024-10-21 21:54:44 +0500

    jq console

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,5 @@
-curl -s https://01.tomorrow-school.ai/assets/superhero/all.json | jq -r --arg id "$HERO_ID" '.[] | select(.id == $id) | .connections.relatives'
+if [ -z "$HERO_ID" ]; then
+    break;
+else
+    curl -s https://01.tomorrow-school.ai/assets/superhero/all.json | jq -r --arg id "$HERO_ID" '.[] | select(.id == $id) | .connections.relatives'
+fi
