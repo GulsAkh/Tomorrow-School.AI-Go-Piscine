9049151aed5cfe2dbff3f78051704075c3de52a5
Guest User <Guest@TCC-09.local>
2024-10-23 15:59:48 +0500

    3 digits

--- printcomb.go
+++ printcomb.go
@@ -7,10 +7,10 @@ import (
 func PrintComb() {
 	for i := 0; i <= 7; i++ {
 		for x := i + 1; x <= 8; x++ {
-			for y := x + 2; y <= 9; y++ {
-				z01.PrintRune('0' + rune(i))
-				z01.PrintRune('0' + rune(x))
-				z01.PrintRune('0' + rune(y))
+			for y := x + 1; y <= 9; y++ {
+				z01.PrintRune(rune(i + '0'))
+				z01.PrintRune(rune(x + '0'))
+				z01.PrintRune(rune(y + '0'))
 
 				if i < 7 || (i == 7 && x < 8) || (i == 7 && x == 8 && y < 9) {
 					z01.PrintRune(',')
