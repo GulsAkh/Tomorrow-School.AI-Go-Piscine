23eb78f02a8f3f18e4e748d46a1f5939863393cf
Guest User <Guest@TCC-09.local>
2024-10-23 14:15:13 +0500

    3 digits

---
+++ printcomb.go
@@ -0,0 +1,24 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintComb() {
+	var i, x, y int
+	for i = 0; i <= 7; i++ {
+		for x = i + 1; x <= 8; x++ {
+			for y = x + 1; y <= 9; y++ {
+				z01.PrintRune(rune(i + '0'))
+				z01.PrintRune(rune(x + '0'))
+				z01.PrintRune(rune(y + '0'))
+
+				if !(i == 7 && x == 8 && y == 9) {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
