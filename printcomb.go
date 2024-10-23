84279e7454420fc351fd7a4ac093daed34c7bc41
Guest User <Guest@TCC-09.local>
2024-10-23 17:10:22 +0500

    3 digits

--- printcomb.go
+++ printcomb.go
@@ -3,20 +3,15 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	var i, x, y int
-	for i = 0; i <= 7; i++ {
-		for x = i + 1; x <= 8; x++ {
-			for y = x + 1; y <= 9; y++ {
+	for i := '0'; i <= '7'; i++ {
+		for x := i + 1; x <= '8'; x++ {
+			for y := x + 1; y <= '9'; y++ {
 
-				firstDigit := '0' + rune(i)
-				secondDigit := '0' + rune(x)
-				thirdDigit := '0' + rune(y)
+				z01.PrintRune(i)
+				z01.PrintRune(x)
+				z01.PrintRune(y)
 
-				z01.PrintRune(firstDigit)
-				z01.PrintRune(secondDigit)
-				z01.PrintRune(thirdDigit)
-
-				if i < 7 || (i == 7 && x < 8) || (i == 7 && x == 8 && y < 9) {
+				if i < '7' || (i == '7' && x < '8') || (i == '7' && x == '8' && y < '9') {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
