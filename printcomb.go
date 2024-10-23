9a2a07c6327a7f0d47abfd08c685d9b0f626de8b
Guest User <Guest@TCC-09.local>
2024-10-23 15:31:50 +0500

    3 digits

--- printcomb.go
+++ printcomb.go
@@ -6,9 +6,9 @@ import (
 
 func PrintComb() {
 	var i, x, y int
-	for i = 0; i <= 7; i++ {
-		for x = i + 1; x <= 8; x++ {
-			for y = x + 1; y <= 9; y++ {
+	for i = 0; i <= 1; i++ {
+		for x = 1; x <= 2; x++ {
+			for y = 2; y <= 3; y++ {
 				z01.PrintRune(rune(i + '0'))
 				z01.PrintRune(rune(x + '0'))
 				z01.PrintRune(rune(y + '0'))
@@ -18,7 +18,9 @@ func PrintComb() {
 					z01.PrintRune(' ')
 				}
 			}
+			y = y + 1
 		}
+		x = x + 1
 	}
 	z01.PrintRune('\n')
 }
