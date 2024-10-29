b1fd7663690f2f850450a728385acec552b27211
Guest User <Guest@TCC-09.local>
2024-10-29 14:42:11 +0500

    N rune

--- nrune.go
+++ nrune.go
@@ -1,8 +1,4 @@
-package main
-
-import (
-	"github.com/01-edu/z01"
-)
+package piscine
 
 func NRune(s string, n int) rune {
 	var runes rune
@@ -10,13 +6,4 @@ func NRune(s string, n int) rune {
 		runes = rune(s[n-1])
 	}
 	return runes
-}
-
-func main() {
-	// z01.PrintRune(NRune("Hello!", 3))
-	// z01.PrintRune(NRune("Salut!", 2))
-	// z01.PrintRune(NRune("Bye!", -1))
-	// z01.PrintRune(NRune("Bye!", 5))
-	z01.PrintRune(NRune("Ola!", 4))
-	z01.PrintRune('\n')
 }
