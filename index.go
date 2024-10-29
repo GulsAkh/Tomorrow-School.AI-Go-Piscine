3aafe2cbe5dd95da8f37805270b1052113d90f99
Guest User <Guest@TCC-09.local>
2024-10-29 16:18:14 +0500

    index

--- index.go
+++ index.go
@@ -2,6 +2,9 @@ package piscine
 
 func Index(s string, toFind string) int {
 	var index int = 0
+	if len(toFind) == 0 {
+		return 0
+	}
 	for i := 0; i < len(s); i++ {
 		if s[i] == toFind[0] {
 			index = i
