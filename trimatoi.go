08f3b349eb7f66fb5e1fe6e63d30df9cac26aecd
Guest User <Guest@TCC-09.local>
2024-10-29 21:17:56 +0500

    atoi

--- trimatoi.go
+++ trimatoi.go
@@ -3,11 +3,13 @@ package piscine
 func TrimAtoi(s string) int {
 	num := 0
 	sign := 1
+	flag := false
 
 	for _, el := range s {
 		if el >= '1' && el <= '9' {
 			num = num*10 + int(el-'0')
-		} else if el == '-' {
+			flag = true
+		} else if el == '-' && !flag {
 			sign = -1
 		}
 	}
