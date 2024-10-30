7e1f247749b42793c2fd989ce8f3db65fc44f504
Guest User <Guest@TCC-09.local>
2024-10-30 12:47:08 +0500

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,18 +1,30 @@
-package main
+package piscine
 
-import "fmt"
-
-func Capitalize(s string) {
-	var str []string
-
-	for _, el := range s {
-		if (el >= '0' && el <= '9') || (el >= 'A' && el <= 'Z') || (el >= 'a' && el <= 'z') {
-			str = append(str, string(el))
+func Capitalize(s string) string {
+	runes := []rune(s)
+	str := ""
+	if s == "" {
+		return ""
+	}
+	if runes[0] >= 'a' && runes[0] <= 'z' {
+		str += string(runes[0] - 32)
+	} else {
+		str += string(runes[0])
+	}
+	for i := 1; i < len(runes); i++ {
+		if (runes[i-1] < 'A' || runes[i-1] > 'Z') && (runes[i-1] < 'a' || runes[i-1] > 'z') && (runes[i-1] < '0' || runes[i-1] > '9') {
+			if runes[i] >= 'a' && runes[i] <= 'z' {
+				str += string(runes[i] - 32)
+			} else {
+				str += string(runes[i])
+			}
+		} else {
+			if runes[i] >= 'A' && runes[i] <= 'Z' {
+				str += string(runes[i] + 32)
+			} else {
+				str += string(runes[i])
+			}
 		}
 	}
-	fmt.Print(str[5])
-}
-
-func main() {
-	Capitalize("Hello! How are you?")
+	return str
 }
