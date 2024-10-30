705eb73f63521ac30e4e9e44314b5b678c2e5e66
Guest User <Guest@TCC-09.local>
2024-10-30 16:35:05 +0500

    print nbr

--- printnbrbase.go
+++ printnbrbase.go
@@ -4,15 +4,11 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintNbrBase(nbr int, base string) {
+func PrintNbrBase(nbr int64, base string) {
 	based := []rune(base)
 	len_base := len(based)
 
 	str := ""
-	if nbr < 0 {
-		z01.PrintRune('-')
-		nbr = -nbr
-	}
 
 	for i := 0; i < len_base; i++ {
 		for j := i + 1; j < len_base; j++ {
@@ -31,6 +27,11 @@ func PrintNbrBase(nbr int, base string) {
 		z01.PrintRune('N')
 		z01.PrintRune('V')
 		return
+	}
+
+	if nbr < 0 {
+		z01.PrintRune('-')
+		nbr = -nbr
 	}
 
 	if nbr == 0 {
