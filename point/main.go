81b2c425cc07dd7cc1ac9d3b8f16638520b34f7b
Guest User <Guest@TCA-11.local>
2024-11-04 19:01:21 +0500

    point

--- point/main.go
+++ point/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
@@ -13,10 +13,41 @@ func setPoint(ptr *point) *point {
 	return ptr
 }
 
+func convertInt(n int) string {
+	var str string
+
+	if n == 0 {
+		str = "0"
+	} else {
+		if n < 0 {
+			str = "-"
+			n = -n
+		}
+
+		for n > 0 {
+			digit := n % 10
+			str = string(rune('0'+digit)) + str
+			n = n / 10
+		}
+	}
+	return str
+}
+
+func printWithRune(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
 func main() {
 	points := point{}
 
 	setPoint(&points)
+	str1 := convertInt(points.x)
+	str2 := convertInt(points.y)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	message := "x = " + str1 + ", " + "y = " + str2
+
+	printWithRune(message)
 }
