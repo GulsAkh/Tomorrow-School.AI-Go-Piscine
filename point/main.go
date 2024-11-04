d3c793e5ede9112bce88fb5b201e3d993ed8b847
Guest User <Guest@TCA-11.local>
2024-11-04 22:03:37 +0500

    point

--- point/main.go
+++ point/main.go
@@ -13,23 +13,10 @@ func setPoint(ptr *point) *point {
 	return ptr
 }
 
-func printNum(num int) {
-	digit := []int{}
-	for num > 0 {
-		digit = append(digit, num%10)
-		num /= 10
-	}
-
-	for i := len(digit) - 1; i >= 0; i-- {
-		z01.PrintRune(rune(48 + digit[i]))
-	}
-}
-
 func main() {
 	points := point{}
 	setPoint(&points)
-	n1 := points.x
-	n2 := points.y
+
 	xChar := 'x'
 	yChar := 'y'
 	equalCh := '='
@@ -41,14 +28,16 @@ func main() {
 	z01.PrintRune(spaceCh)
 	z01.PrintRune(equalCh)
 	z01.PrintRune(spaceCh)
-	printNum(n1)
+	z01.PrintRune('0' + rune(points.x/10))
+	z01.PrintRune('0' + rune(points.x%10))
 	z01.PrintRune(commaCh)
 	z01.PrintRune(spaceCh)
 	z01.PrintRune(yChar)
 	z01.PrintRune(spaceCh)
 	z01.PrintRune(equalCh)
 	z01.PrintRune(spaceCh)
-	printNum(n2)
+	z01.PrintRune('0' + rune(points.y/10))
+	z01.PrintRune('0' + rune(points.y%10))
 
 	z01.PrintRune(newLine)
 }
