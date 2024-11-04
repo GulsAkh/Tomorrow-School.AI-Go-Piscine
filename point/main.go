6dd81c14abb0e217087397f6785757b8975226d2
Guest User <Guest@TCA-11.local>
2024-11-04 22:29:18 +0500

    point

--- point/main.go
+++ point/main.go
@@ -10,34 +10,33 @@ type point struct {
 func setPoint(ptr *point) *point {
 	ptr.x = 42
 	ptr.y = 21
+	xChar := 'x'
+	yChar := 'y'
+	equalCh := '='
+	spaceCh := ' '
+	commaCh := ','
+	newLine := '\n'
+
+	z01.PrintRune(xChar)
+	z01.PrintRune(spaceCh)
+	z01.PrintRune(equalCh)
+	z01.PrintRune(spaceCh)
+	z01.PrintRune(52)
+	z01.PrintRune(50)
+	z01.PrintRune(commaCh)
+	z01.PrintRune(spaceCh)
+	z01.PrintRune(yChar)
+	z01.PrintRune(spaceCh)
+	z01.PrintRune(equalCh)
+	z01.PrintRune(spaceCh)
+	z01.PrintRune(50)
+	z01.PrintRune(49)
+
+	z01.PrintRune(newLine)
 	return ptr
 }
 
 func main() {
 	points := point{}
 	setPoint(&points)
-
-	// xChar := 'x'
-	// yChar := 'y'
-	// equalCh := '='
-	// spaceCh := ' '
-	// commaCh := ','
-	// newLine := '\n'
-
-	// z01.PrintRune(xChar)
-	// z01.PrintRune(spaceCh)
-	// z01.PrintRune(equalCh)
-	// z01.PrintRune(spaceCh)
-	z01.PrintRune(52)
-	z01.PrintRune(50)
-	// z01.PrintRune(commaCh)
-	// z01.PrintRune(spaceCh)
-	// z01.PrintRune(yChar)
-	// z01.PrintRune(spaceCh)
-	// z01.PrintRune(equalCh)
-	// z01.PrintRune(spaceCh)
-	z01.PrintRune(50)
-	z01.PrintRune(49)
-
-	// z01.PrintRune(newLine)
 }
