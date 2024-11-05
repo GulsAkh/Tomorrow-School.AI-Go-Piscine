20d893b402e41cc9458a5da0f93e5a50fefd83c1
Guest User <Guest@TCA-11.local>
2024-11-05 14:24:28 +0500

    point

--- point/main.go
+++ point/main.go
@@ -19,11 +19,22 @@ func main() {
 	for _, chr := range "x = " {
 		z01.PrintRune(chr)
 	}
-	z01.PrintRune(52)
-	z01.PrintRune(50)
-	for _, chr := range "y = " {
+	for i := 0; i < 2; i++ {
+		if i == 0 {
+			z01.PrintRune(rune('0' + points.x/10))
+		} else {
+			z01.PrintRune(rune('0' + points.x%10))
+		}
+	}
+
+	for _, chr := range ", y = " {
 		z01.PrintRune(chr)
 	}
-	z01.PrintRune(50)
-	z01.PrintRune(49)
+	for i := 0; i < 2; i++ {
+		if i == 0 {
+			z01.PrintRune(rune('0' + points.y/10))
+		} else {
+			z01.PrintRune(rune('0' + points.y%10))
+		}
+	}
 }
