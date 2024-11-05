dc94d554f4ae374127e0ca7834e8d863b031e007
Guest User <Guest@TCA-11.local>
2024-11-05 13:19:13 +0500

    point

--- point/main.go
+++ point/main.go
@@ -10,33 +10,13 @@ type point struct {
 func setPoint(ptr *point) *point {
 	ptr.x = 42
 	ptr.y = 21
-	xChar := 'x'
-	yChar := 'y'
-	equalCh := '='
-	spaceCh := ' '
-	commaCh := ','
-	newLine := '\n'
-
-	z01.PrintRune(xChar)
-	z01.PrintRune(spaceCh)
-	z01.PrintRune(equalCh)
-	z01.PrintRune(spaceCh)
-	z01.PrintRune(52)
-	z01.PrintRune(50)
-	z01.PrintRune(commaCh)
-	z01.PrintRune(spaceCh)
-	z01.PrintRune(yChar)
-	z01.PrintRune(spaceCh)
-	z01.PrintRune(equalCh)
-	z01.PrintRune(spaceCh)
-	z01.PrintRune(50)
-	z01.PrintRune(49)
-
-	z01.PrintRune(newLine)
 	return ptr
 }
 
 func main() {
 	points := point{}
 	setPoint(&points)
+	for _, chr := range "x = 42, y = 21\n" {
+		z01.PrintRune(chr)
+	}
 }
