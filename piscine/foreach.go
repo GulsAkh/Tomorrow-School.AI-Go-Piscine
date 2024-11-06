82208b3d736b2cd9ef9c86e7553efee324ef93c3
Guest User <Guest@TCA-11.local>
2024-11-06 16:05:35 +0500

    function

---
+++ piscine/foreach.go
@@ -0,0 +1,16 @@
+package piscine
+
+import (
+	"fmt"
+)
+
+func PrintNbr(num int) {
+	fmt.Print(num)
+}
+
+func ForEach(f func(int), a []int) {
+	for _, el := range a {
+		f(el)
+	}
+	fmt.Println()
+}
