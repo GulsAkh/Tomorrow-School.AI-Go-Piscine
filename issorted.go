c48c699cab28948898ac4455f1adf08c1449114a
Guest User <Guest@TCA-11.local>
2024-11-06 21:40:09 +0500

    sorted

---
+++ issorted.go
@@ -0,0 +1,21 @@
+package piscine
+
+func f(n1, n2 int) int {
+	if n1 > n2 {
+		return 1
+	} else if n1 == n2 {
+		return 0
+	}
+	return -1
+}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i < len(a)-1; i++ {
+		b := a[i]
+		c := a[i+1]
+		if (f(b, c)) > 0 {
+			return false
+		}
+	}
+	return true
+}
