8046eb39a102d54573b025fae368a927a220adb9
Guest User <Guest@TCA-11.local>
2024-11-06 21:59:16 +0500

    sorted

--- issorted.go
+++ issorted.go
@@ -1,18 +1,10 @@
 package piscine
 
 func f(n1, n2 int) int {
-	if n1 < 0 && n2 < 0 {
-		if n1 < n2 {
-			return -1
-		}
-	} else if n1 > 0 && n2 < 0 {
-		return -1
-	} else {
-		if n1 > n2 {
-			return 1
-		} else if n1 == n2 {
-			return 0
-		}
+	if n1 > n2 {
+		return 1
+	} else if n1 == n2 {
+		return 0
 	}
 	return -1
 }
@@ -20,8 +12,8 @@ func f(n1, n2 int) int {
 func IsSorted(f func(a, b int) int, a []int) bool {
 	flag := true
 	for i := 0; i < len(a)-1; i++ {
-		b := a[i]
-		c := a[i+1]
+		b := a[i] % 10
+		c := a[i+1] % 10
 		if (f(b, c)) > 0 {
 			flag = false
 		}
