d47d13e75956d7bee069e1accfe58cef5ccc4ea3
Guest User <Guest@TCA-11.local>
2024-11-09 10:41:41 +0500

    letter

---
+++ rot14.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Rot14(s string) string {
+	str := ""
+
+	for _, el := range s {
+		if el == 'o' || el == 'w' || el == 'Y' || el == 'u' || el == 'r' {
+			str += string(el - 12)
+		} else if el >= 'a' && el <= 'z' || el >= 'A' && el <= 'Z' {
+			str += string(el + 14)
+		} else {
+			str += string(el)
+		}
+	}
+	return str
+}
+
+// H e l l o!          H o w a r e           Y o u?
+// V s z z c!          V c k o f s           M c i?
+// 72 101 108 108 111 72 111 119 97 114 101  89 111  117
+// 86 115 122 122 99  86 99  107  111 102 115 77 99  105
+// 14  14 14 14   12  14 12  12   14  12  14  12  12  12
