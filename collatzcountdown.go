ff2bfda12a7d4a4aec2786997803bad71cdccc1f
Guest User <Guest@TCA-11.local>
2024-11-09 14:10:19 +0500

    collatz

---
+++ collatzcountdown.go
@@ -0,0 +1,23 @@
+package main
+
+func CollatzCountdown(start int) int {
+	count := 0
+	if start <= 0 {
+		return -1
+	}
+	for start != 1 {
+		if start%2 == 0 {
+			start /= 2
+			count++
+		} else if start%2 == 1 {
+			start = start*3 + 1
+			count++
+		}
+	}
+	return count
+}
+
+// func main() {
+// 	steps := CollatzCountdown(12)
+// 	fmt.Println(steps)
+// }
