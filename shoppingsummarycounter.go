3c3e2a5ca53731cff9e38515d40cf32dca6e7352
Guest User <Guest@TCA-11.local>
2024-11-09 16:49:46 +0500

    shopping

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,33 @@
+package piscine
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	m := make(map[string]int)
+	str1 := ""
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+		if char == ' ' {
+			if str != "" {
+				m[str1]++
+				str1 = ""
+			} else {
+				m[" "]++
+			}
+		} else {
+			str1 += string(char)
+		}
+	}
+
+	if str != "" {
+		m[str1]++
+		str1 = ""
+	}
+
+	return m
+}
+
+// func main() {
+// 	summary := "Burger Water Carrot Coffee Water Water Chips Carrot Carrot Burger Carrot Water"
+// 	for index, element := range ShoppingSummaryCounter(summary) {
+// 		fmt.Println(index, "=>", element)
+// 	}
+// }
