b27bb1d127e1d3e27b239b3cb8c41032f86c08c6
Guest User <Guest@TCA-11.local>
2024-11-09 18:37:53 +0500

    cards.h

---
+++ dealapackofcards.go
@@ -0,0 +1,39 @@
+package piscine
+
+import "fmt"
+
+func printNum(cards []int) {
+	for i, card := range cards {
+		if i > 0 {
+			fmt.Printf(" ")
+		}
+		fmt.Printf("%d", card)
+	}
+	fmt.Printf("\n")
+}
+
+func DealAPackOfCards(deck []int) {
+	n := len(deck) / 4
+
+	player1 := (deck[:n])
+	player2 := deck[n : 2*n]
+	player3 := deck[2*n : 3*n]
+	player4 := deck[3*n:]
+
+	fmt.Printf("Player 1: ")
+	printNum(player1)
+
+	fmt.Printf("Player 2: ")
+	printNum(player2)
+
+	fmt.Printf("Player 3: ")
+	printNum(player3)
+
+	fmt.Printf("Player 4: ")
+	printNum(player4)
+}
+
+// func main() {
+// 	deck := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+// 	DealAPackOfCards(deck)
+// }
