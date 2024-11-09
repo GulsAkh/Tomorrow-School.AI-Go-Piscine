e9ed41c3c53b0814738c60a3226c1e3d71f647a1
Guest User <Guest@TCA-11.local>
2024-11-09 15:32:59 +0500

    abort

--- abort.go
+++ abort.go
@@ -1,10 +1,20 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	return (a + b + c + d + e) / 5
+	num := []int{a, b, c, d, e}
+
+	for i := 0; i < len(num); i++ {
+		for j := i + 1; j < len(num); j++ {
+			if num[i] > num[j] {
+				num[i], num[j] = num[j], num[i]
+			}
+		}
+	}
+
+	return num[2]
 }
 
 // func main() {
-// 	middle := Abort(2, 3, 8, 5, 7)
+// 	middle := Abort(8206158666394987370, -7726695374529039112, 4647475675926194097, -5217061567791790716, -3643945018500837920)
 // 	fmt.Println(middle)
 // }
