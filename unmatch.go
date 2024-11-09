5ea923cdb4e49c0c34d1a93cb678b3a74f11e205
Guest User <Guest@TCA-11.local>
2024-11-09 12:56:20 +0500

    unmatch

--- unmatch.go
+++ unmatch.go
@@ -1,21 +1,20 @@
 package piscine
 
 func Unmatch(a []int) int {
-	// Map to store the frequency of each element
 	c := make(map[int]int)
 	for _, num := range a {
 		c[num]++
 	}
-
-	// Check for elements with odd occurrences
+	var num int
 	for _, count := range c {
 		if c[count]%2 != 0 {
-			return count // Return the first element with an odd count (no pair)
+			num = count // Return the first element with an odd count (no pair)
+		} else {
+			num = -1
 		}
 	}
 
-	// If all elements have pairs, return -1
-	return -1
+	return num
 }
 
 // func main() {
