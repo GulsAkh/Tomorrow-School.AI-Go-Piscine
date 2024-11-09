53508072644929c0fcafd65a12684f6c06ffdca0
Guest User <Guest@TCA-11.local>
2024-11-10 01:06:19 +0500

    galaxy.h

---
+++ comcheck/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	for _, el := range args {
+		if el == "01" || el == "galaxy" || el == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
