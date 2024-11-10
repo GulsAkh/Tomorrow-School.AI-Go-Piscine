63473502afe6500a51b4f7d1fe5ef4c8711efc95
Guest User <Guest@TCA-11.local>
2024-11-10 07:21:58 +0500

    max.h

--- max.go
+++ max.go
@@ -1,20 +1,14 @@
 package piscine
 
 func Max(a []int) int {
-	var maX int
+	maX := a[0]
 	if len(a) == 0 {
 		return 0
 	}
-	for i := 0; i < len(a)-1; i++ {
-		if a[i] > a[i+1] {
+	for i := 0; i < len(a); i++ {
+		if maX < a[i] {
 			maX = a[i]
 		}
 	}
 	return maX
 }
-
-// func main() {
-// 	a := []int{23, 123, 1, 11, 55, 93}
-// 	max := Max(a)
-// 	fmt.Println(max)
-// }
