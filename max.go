452f43516e34338777a0f9a2bd0b3b0467c5e7a2
Guest User <Guest@TCA-11.local>
2024-11-10 07:08:14 +0500

    max.h

---
+++ max.go
@@ -0,0 +1,20 @@
+package piscinego
+
+func Max(a []int) int {
+	var maX int
+	if len(a) == 0 {
+		return 0
+	}
+	for i := 0; i < len(a)-1; i++ {
+		if a[i] > a[i+1] {
+			maX = a[i]
+		}
+	}
+	return maX
+}
+
+// func main() {
+// 	a := []int{23, 123, 1, 11, 55, 93}
+// 	max := Max(a)
+// 	fmt.Println(max)
+// }
