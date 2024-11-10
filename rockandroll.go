a6ccb0157e15d4dff7d3f760697388ff98302b2c
Guest User <Guest@TCA-11.local>
2024-11-10 09:00:44 +0500

    rcok

--- rockandroll.go
+++ rockandroll.go
@@ -1,20 +1,17 @@
 package piscine
 
 func RockAndRoll(n int) string {
-	str := ""
 	if n < 0 {
-		str += "number is negative\n"
+		return "number is negative\n"
 	}
 	if n%2 == 0 && n%3 == 0 {
-		str += "rock and roll\n"
+		return "rock and roll\n"
 	} else if n%2 == 0 {
-		str += "rock\n"
+		return "rock\n"
 	} else if n%3 == 0 {
-		str += "roll\n"
-	} else {
-		str += "non divisible\n"
+		return "roll\n"
 	}
-	return str
+	return "non divisible"
 }
 
 // func main() {
