d9b9acaf83d5b88583b86a8f62f530632e4e8f78
Guest User <Guest@TCA-11.local>
2024-11-11 19:37:44 +0500

    clearlistQ11

---
+++ listclear.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+type (
+	List = piscine.List
+	Node = piscine.NodeL
+)
+
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{}
+	newNode.Data = data
+	newNode.Next = nil
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
+
+func PrintList(l *List) {
+	link := l.Head
+	for link != nil {
+		fmt.Print(link.Data, " -> ")
+		link = link.Next
+	}
+	fmt.Println(nil)
+}
+
+func ListClear(l *List) {
+	for l.Head != nil {
+		l.Head = l.Head.Next
+	}
+}
+
+func main() {
+	link := &List{}
+	ListPushBack(link, "I")
+	ListPushBack(link, 1)
+	ListPushBack(link, "something")
+	ListPushBack(link, 2)
+
+	fmt.Println("------list------")
+	PrintList(link)
+	ListClear(link)
+	fmt.Println("------updated list------")
+	PrintList(link)
+}
--- listlast.go
+++ listlast.go
@@ -1,28 +1,28 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
-func ListPushBack(l *List, data interface{}) {
-	newNode := &NodeL{}
-	newNode.Data = data
-	newNode.Next = nil
+// func ListPushBack(l *List, data interface{}) {
+// 	newNode := &NodeL{}
+// 	newNode.Data = data
+// 	newNode.Next = nil
 
-	if l.Head == nil {
-		l.Head = newNode
-		l.Tail = newNode
-	} else {
-		l.Tail.Next = newNode
-		l.Tail = newNode
-	}
-}
+// 	if l.Head == nil {
+// 		l.Head = newNode
+// 		l.Tail = newNode
+// 	} else {
+// 		l.Tail.Next = newNode
+// 		l.Tail = newNode
+// 	}
+// }
 
 func ListLast(l *List) interface{} {
 	if l.Head == nil {
