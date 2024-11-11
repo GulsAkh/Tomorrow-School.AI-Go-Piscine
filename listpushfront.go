a8141d36eefccb2b59c3336142529244428218c7
Guest User <Guest@TCA-11.local>
2024-11-11 17:22:37 +0500

    push frontQ11

---
+++ listpushfront.go
@@ -0,0 +1,20 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	newNode := &NodeL{}
+	newNode.Data = data
+	newNode.Next = l.Head
+
+	l.Head = newNode
+	l.Tail = newNode
+}
