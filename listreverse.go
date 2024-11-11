890a03ea4156c75e8f970719d5ad65719c257899
Guest User <Guest@TCA-11.local>
2024-11-11 21:46:37 +0500

    reverse

---
+++ listreverse.go
@@ -0,0 +1,59 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+// func ListPushBack(l *List, data interface{}) {
+// 	newNode := &NodeL{}
+// 	newNode.Data = data
+// 	newNode.Next = nil
+
+// 	if l.Head == nil {
+// 		l.Head = newNode
+// 		l.Tail = newNode
+// 	} else {
+// 		l.Tail.Next = newNode
+// 		l.Tail = newNode
+// 	}
+// }
+
+func ListReverse(l *List) {
+	var prev, next *NodeL
+	current := l.Head
+	l.Tail = l.Head
+	for current != nil {
+		next = current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	l.Head = prev
+}
+
+// func main() {
+// 	link := &List{}
+
+// 	ListPushBack(link, 1)
+// 	ListPushBack(link, 2)
+// 	ListPushBack(link, 3)
+// 	ListPushBack(link, 4)
+
+// 	ListReverse(link)
+
+// 	it := link.Head
+
+// 	for it != nil {
+// 		fmt.Println(it.Data)
+// 		it = it.Next
+// 	}
+
+// 	fmt.Println("Tail", link.Tail)
+// 	fmt.Println("Head", link.Head)
+// }
