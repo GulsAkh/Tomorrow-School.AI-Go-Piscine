321439121b8dfe7ba2ae931fc94a0905768c8a62
Guest User <Guest@TCA-11.local>
2024-11-11 17:52:39 +0500

    listsizeQ11

--- listpushfront.go
+++ listpushfront.go
@@ -10,11 +10,11 @@ package piscine
 // 	Tail *NodeL
 // }
 
-func ListPushFront(l *List, data interface{}) {
-	newNode := &NodeL{}
-	newNode.Data = data
-	newNode.Next = l.Head
+// func ListPushFront(l *List, data interface{}) {
+// 	newNode := &NodeL{}
+// 	newNode.Data = data
+// 	newNode.Next = l.Head
 
-	l.Head = newNode
-	l.Tail = newNode
-}
+// 	l.Head = newNode
+// 	l.Tail = newNode
+// }
