d8cf4d755e2f4879d11a97855817469e431f854a
Guest User <Guest@TCA-11.local>
2024-11-11 17:51:45 +0500

    listsizeQ11

---
+++ listsize.go
@@ -0,0 +1,35 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	newNode := &NodeL{}
+	newNode.Data = data
+	newNode.Next = nil
+
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
+}
+
+func ListSize(l *List) int {
+	count := 0
+
+	for l.Head != nil {
+		count++
+		l.Head = l.Head.Next
+	}
+	return count
+}
