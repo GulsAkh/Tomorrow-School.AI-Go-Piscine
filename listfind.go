e5630155e3ab92e1b6692c99039575dc2fb7a3b1
Guest User <Guest@TCA-11.local>
2024-11-12 18:40:03 +0500

    listfindQ11

---
+++ listfind.go
@@ -0,0 +1,58 @@
+package piscine
+
+// import (
+// 	"fmt"
+// )
+
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
+
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
+
+// func ListPushBack(l *List, data interface{}) {
+// 	node := &NodeL{}
+// 	node.Data = data
+// 	node.Next = nil
+
+// 	if l.Head == nil {
+// 		l.Head = node
+// 		l.Tail = node
+// 	} else {
+// 		l.Tail.Next = node
+// 		l.Tail = node
+// 	}
+// }
+
+// func CompStr(a, b interface{}) bool {
+// 	return a == b
+// }
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	current := l.Head
+	for current != nil {
+		if comp(current.Data, ref) {
+			return &current.Data
+		}
+		current = current.Next
+	}
+	return nil
+}
+
+// func main() {
+// 	link := &List{}
+
+// 	ListPushBack(link, "hello")
+// 	ListPushBack(link, "hello1")
+// 	ListPushBack(link, "hello2")
+// 	ListPushBack(link, "hello3")
+
+// 	found := ListFind(link, interface{}("hello2"), CompStr)
+
+// 	fmt.Println(found)
+// 	fmt.Println(*found)
+// }
