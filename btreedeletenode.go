cbc39065181726a7ad884f17f2498f78f07c02bd
Guest User <Guest@TCA-11.local>
2024-11-14 20:50:00 +0500

    DeleteQ12

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -1,75 +1,75 @@
 package piscine
 
-// func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-// 	if root == nil {
-// 		return nil
-// 	}
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
 
-// 	if elem == root.Data {
-// 		return root
-// 	}
+	if elem == root.Data {
+		return root
+	}
 
-// 	if elem < root.Data {
-// 		return BTreeSearchItem(root.Left, elem)
-// 	} else {
-// 		return BTreeSearchItem(root.Right, elem)
-// 	}
-// }
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	} else {
+		return BTreeSearchItem(root.Right, elem)
+	}
+}
 
-// func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
-// 	if root == nil {
-// 		return
-// 	}
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
 
-// 	BTreeApplyInorder(root.Left, f)
-// 	f(root.Data)
-// 	BTreeApplyInorder(root.Right, f)
-// }
+	BTreeApplyInorder(root.Left, f)
+	f(root.Data)
+	BTreeApplyInorder(root.Right, f)
+}
 
-// func findMin(node *TreeNode) *TreeNode {
-// 	current := node
-// 	for current.Left != nil {
-// 		current = current.Left
-// 	}
-// 	return current
-// }
+func findMin(node *TreeNode) *TreeNode {
+	current := node
+	for current.Left != nil {
+		current = current.Left
+	}
+	return current
+}
 
-// func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
-// 	if node.Left == nil && node.Right == nil {
-// 		if node.Parent == nil { // node is root
-// 			return nil
-// 		}
-// 		if node.Parent.Left == node {
-// 			node.Parent.Left = nil
-// 		} else {
-// 			node.Parent.Right = nil
-// 		}
-// 		return root
-// 	}
+func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
+	if node.Left == nil && node.Right == nil {
+		if node.Parent == nil { // node is root
+			return nil
+		}
+		if node.Parent.Left == node {
+			node.Parent.Left = nil
+		} else {
+			node.Parent.Right = nil
+		}
+		return root
+	}
 
-// 	if node.Left == nil || node.Right == nil {
-// 		var child *TreeNode
-// 		if node.Left != nil {
-// 			child = node.Left
-// 		} else {
-// 			child = node.Right
-// 		}
+	if node.Left == nil || node.Right == nil {
+		var child *TreeNode
+		if node.Left != nil {
+			child = node.Left
+		} else {
+			child = node.Right
+		}
 
-// 		if node.Parent == nil {
-// 			child.Parent = nil
-// 			return child
-// 		}
+		if node.Parent == nil {
+			child.Parent = nil
+			return child
+		}
 
-// 		if node.Parent.Left == node {
-// 			node.Parent.Left = child
-// 		} else {
-// 			node.Parent.Right = child
-// 		}
-// 		child.Parent = node.Parent
-// 		return root
-// 	}
+		if node.Parent.Left == node {
+			node.Parent.Left = child
+		} else {
+			node.Parent.Right = child
+		}
+		child.Parent = node.Parent
+		return root
+	}
 
-// 	successor := findMin(node.Right)
-// 	node.Data = successor.Data
-// 	return BTreeDeleteNode(root, successor)
-// }
+	successor := findMin(node.Right)
+	node.Data = successor.Data
+	return BTreeDeleteNode(root, successor)
+}
