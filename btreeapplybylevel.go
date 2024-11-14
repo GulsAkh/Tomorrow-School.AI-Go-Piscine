e24ed80d6ad6a4ec87860851fef3841e51a27b63
Guest User <Guest@TCA-11.local>
2024-11-14 19:49:06 +0500

    Commented

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -11,10 +11,17 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
+
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
+		if root.Left != nil {
+			root.Left.Parent = root
+		}
 	} else {
 		root.Right = BTreeInsertData(root.Right, data)
+		if root.Right != nil {
+			root.Right.Parent = root
+		}
 	}
 	return root
 }
