66421a41ae0d1197ad5605a7b3f7549741ceed48
Guest User <Guest@TCA-11.local>
2024-11-14 16:59:12 +0500

    maxQ12

--- btreemax.go
+++ btreemax.go
@@ -20,12 +20,13 @@ package piscine
 // }
 
 func BTreeMax(root *TreeNode) *TreeNode {
-	maX := root
+	if root == nil {
+		return nil
+	}
 	for root.Right != nil {
 		root = root.Right
-		maX = root
 	}
-	return maX
+	return root
 }
 
 // func main() {
