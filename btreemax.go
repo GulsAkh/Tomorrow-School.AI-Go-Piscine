fa877df1e6f0a5927de94a37a99b7fe52097f2d9
Guest User <Guest@TCA-11.local>
2024-11-14 18:50:13 +0500

    maxQ12

--- btreemax.go
+++ btreemax.go
@@ -1,24 +1,5 @@
 package piscine
 
-// import "fmt"
-
-// type TreeNode struct {
-// 	Data        string
-// 	Left, Right *TreeNode
-// }
-
-// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-// 	if root == nil {
-// 		return &TreeNode{Data: data}
-// 	}
-// 	if data < root.Data {
-// 		root.Left = BTreeInsertData(root.Left, data)
-// 	} else {
-// 		root.Right = BTreeInsertData(root.Right, data)
-// 	}
-// 	return root
-// }
-
 func BTreeMax(root *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
@@ -28,12 +9,3 @@ func BTreeMax(root *TreeNode) *TreeNode {
 	}
 	return BTreeMax(root.Right)
 }
-
-// func main() {
-// 	root := &TreeNode{Data: "4"}
-// 	BTreeInsertData(root, "1")
-// 	BTreeInsertData(root, "7")
-// 	BTreeInsertData(root, "5")
-// 	max := BTreeMax(root)
-// 	fmt.Println(max.Data)
-// }
