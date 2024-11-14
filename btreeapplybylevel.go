4936a5c7ab81d46403416ca049dedc225b8353ea
Guest User <Guest@TCA-11.local>
2024-11-14 20:18:19 +0500

    BTTransQ12

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,49 +1,42 @@
-package piscine
+// package piscine
 
 // import "fmt"
 
-type TreeNode struct {
-	Data                string
-	Left, Right, Parent *TreeNode
-}
+// type TreeNode struct {
+// 	Data        string
+// 	Left, Right *TreeNode
+// }
 
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
-	}
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 	} else {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 	}
+// 	return root
+// }
 
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		if root.Left != nil {
-			root.Left.Parent = root
-		}
-	} else {
-		root.Right = BTreeInsertData(root.Right, data)
-		if root.Right != nil {
-			root.Right.Parent = root
-		}
-	}
-	return root
-}
-
-func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root == nil {
-		return
-	}
-	lst := []*TreeNode{root}
-	i := 0
-	for i < len(lst) {
-		node := lst[i]
-		f(node.Data)
-		i++
-		if node.Left != nil {
-			lst = append(lst, node.Left)
-		}
-		if node.Right != nil {
-			lst = append(lst, node.Right)
-		}
-	}
-}
+// func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+// 	if root == nil {
+// 		return
+// 	}
+// 	lst := []*TreeNode{root}
+// 	i := 0
+// 	for i < len(lst) {
+// 		node := lst[i]
+// 		f(node.Data)
+// 		i++
+// 		if node.Left != nil {
+// 			lst = append(lst, node.Left)
+// 		}
+// 		if node.Right != nil {
+// 			lst = append(lst, node.Right)
+// 		}
+// 	}
+// }
 
 // func main() {
 // 	root := &TreeNode{Data: "4"}
