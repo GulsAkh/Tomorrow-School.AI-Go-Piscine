934758e456a9eca4586545723057dddc755d1c6c
Guest User <Guest@TCA-11.local>
2024-11-14 15:29:06 +0500

    BTByLevelQ12

---
+++ btreeapplybylevel.go
@@ -0,0 +1,40 @@
+package piscine
+
+// import "fmt"
+
+// type TreeNode struct {
+// 	Data        string
+// 	Left, Right *TreeNode
+// }
+
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 	} else {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 	}
+// 	return root
+// }
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root != nil {
+		f(root.Data)
+		if root.Left != nil {
+			BTreeApplyByLevel(root.Left, f)
+		}
+		if root.Right != nil {
+			BTreeApplyByLevel(root.Right, f)
+		}
+	}
+}
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	BTreeApplyByLevel(root, fmt.Println)
+// }
