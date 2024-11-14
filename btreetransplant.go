bc3e081115864836e8f2cccba36c6140c15a11ea
Guest User <Guest@TCA-11.local>
2024-11-14 20:54:01 +0500

    BTTrans

--- btreetransplant.go
+++ btreetransplant.go
@@ -5,25 +5,25 @@ type TreeNode struct {
 	Left, Right, Parent *TreeNode
 }
 
-// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-// 	if root == nil {
-// 		return &TreeNode{Data: data}
-// 	}
-// 	if data < root.Data {
-// 		if root.Left == nil {
-// 			root.Left = &TreeNode{Data: data, Parent: root}
-// 		} else {
-// 			BTreeInsertData(root.Left, data)
-// 		}
-// 	} else {
-// 		if root.Right == nil {
-// 			root.Right = &TreeNode{Data: data, Parent: root}
-// 		} else {
-// 			BTreeInsertData(root.Right, data)
-// 		}
-// 	}
-// 	return root
-// }
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		if root.Left == nil {
+			root.Left = &TreeNode{Data: data, Parent: root}
+		} else {
+			BTreeInsertData(root.Left, data)
+		}
+	} else {
+		if root.Right == nil {
+			root.Right = &TreeNode{Data: data, Parent: root}
+		} else {
+			BTreeInsertData(root.Right, data)
+		}
+	}
+	return root
+}
 
 // func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 // 	if root == nil {
